5038dfc32e7aa26ded803eaa5e5fec928648e2cd
soliury <lingyong1992@foxmail.com>
2018-11-05 22:04:40 +0800

    update sbp

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,6 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlay, faUndo } from '@fortawesome/free-solid-svg-icons'
 import 'whatwg-fetch'
 
+let mathjaxLoaded = false
+function loadingMathjax() {
+  if (mathjaxLoaded) return
+  // mathjax
+  const script = document.createElement('script')
+  script.setAttribute('type', 'text/javascript')
+  script.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-MML-AM_CHTML')
+  document.getElementsByTagName('body')[0].appendChild(script)
+  mathjaxLoaded = true
+}
+
 export default ({Vue, options, router, siteData}) => {
   library.add(faPlay, faUndo)
 
@@ -14,6 +25,7 @@ export default ({Vue, options, router, siteData}) => {
     require('codemirror/mode/javascript/javascript.js')
     Vue.use(Toasted.default)
     Vue.use(VueCodemirror)
+    loadingMathjax()
   }
   Vue.component('v-icon', FontAwesomeIcon)
 }
--- docs/.vuepress/markdown/index.js
+++ docs/.vuepress/markdown/index.js
@@ -1,5 +1,7 @@
+const markdownItMathjax = require('markdown-it-mathjax')
 const demo = require('./demo')
 
 module.exports = md => {
   md.use(demo)
+  md.use(markdownItMathjax())
 };
